573f65b11c0ec32dd092bc5f7dd8391d9f6c4c5e
Doinggithub14 <rewavadnere17@gmail.com>
2025-04-17 20:48:33 +0530

    pesticide recommender added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import HeroSection from "@/components/HeroSection";
+import PesticideRecommender from "@/components/PesticideRecommender";
 import { useState } from "react";
 
 export default function HomePage() {
@@ -37,12 +38,12 @@ export default function HomePage() {
         <HeroSection />
       </section>
 
-      <div className="max-w-xl mx-auto p-4">
+      {/* <div className="max-w-xl mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">
           Pesticide Recommender for Farmers
         </h1>
 
-        <div className="mb-4">
+        <div className="mt-20 mb-4">
           <input
             type="text"
             placeholder="Search crop..."
@@ -56,10 +57,13 @@ export default function HomePage() {
             Search
           </button>
         </div>
-
         {loading && <p>Loading...</p>}
 
         {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
+      </div> */}
+
+      <div className="max-w-4xl mx-auto p-4 mt-10">
+        <PesticideRecommender />
       </div>
     </main>
   );
